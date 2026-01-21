102dd14438483a554eae246b2b3350634665e4fd
daniichiy <ichiydani@gmail.com>
2026-01-21 15:58:49 -0400

    feat: add read categories function

---
+++ core/category.php
@@ -0,0 +1,32 @@
+<?php
+
+class Category {
+    //db stuff
+    private $conn;
+    private $table = 'categories';
+
+    //category properties
+    public $id;
+    public $name;
+    public $created_at;
+
+    //constructor with db connection
+    public function __construct($db){
+        $this->conn = $db;
+    }
+
+    //getting posts from our database
+    public function read(){
+        //create query
+        $query = 'SELECT
+            *
+            FROM '. $this->table;
+
+        //prepare statemant
+        $stmt = $this->conn->prepare($query);
+        //execute query
+        $stmt->execute();
+
+        return $stmt;
+    }
+}
